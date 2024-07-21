bfca404639d7d17416c714072cba899c5b880abb
Therese-Claire <awonguagabi@gmail.com>
2024-07-21 20:26:36 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python3
 """Module for task 0
-
 Write a script that reads stdin line by line and computes metrics:
-
-Input format:
-<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>.
-(if the format is not this one, the line must be skipped).
-After every 10 lines and/or a keyboard interruption (CTRL + C), print these
-statistics from the beginning:
-    Total file size: File size: <total size>
-    where <total size> is the sum of all previous <file size> (format above)
-    Number of lines by status code:
-        possible status code: 200, 301, 400, 401, 403, 404, 405 and 500.
-        if a status code doesn’t appear or is not an integer, don’t
-        print anything for this status code.
-        format: <status code>: <number>.
-        status codes should be printed in ascending order.
 """
 
 
@@ -23,8 +8,8 @@ import re
 
 
 def extract_input(input_line):
-    '''Extracts sections of a line of an HTTP request log.
-    '''
+    """Extracts sections of a line of an HTTP request log.
+    """
     fp = (
         r'\s*(?P<ip>\S+)\s*',
         r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
@@ -47,8 +32,8 @@ def extract_input(input_line):
 
 
 def print_statistics(total_file_size, status_codes_stats):
-    '''Prints the accumulated statistics of the HTTP request log.
-    '''
+    """Prints the accumulated statistics of the HTTP request log.
+    """
     print('File size: {:d}'.format(total_file_size), flush=True)
     for status_code in sorted(status_codes_stats.keys()):
         num = status_codes_stats.get(status_code, 0)
@@ -57,14 +42,14 @@ def print_statistics(total_file_size, status_codes_stats):
 
 
 def update_metrics(line, total_file_size, status_codes_stats):
-    '''Updates the metrics from a given HTTP request log.
+    """Updates the metrics from a given HTTP request log.
 
     Args:
         line (str): The line of input from which to retrieve the metrics.
 
     Returns:
         int: The new total file size.
-    '''
+    """
     line_info = extract_input(line)
     status_code = line_info.get('status_code', '0')
     if status_code in status_codes_stats.keys():
@@ -73,8 +58,8 @@ def update_metrics(line, total_file_size, status_codes_stats):
 
 
 def run():
-    '''Starts the log parser.
-    '''
+    """Starts the log parser.
+    """
     line_num = 0
     total_file_size = 0
     status_codes_stats = {
